7165f1d6fc7cdcdad80f44c2dc8681af7acdb6b0
usaid AKA <ujedkemal@gmail.com>
2024-01-10 22:32:48 +0700

    functionality

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   mobileNav.addEventListener("click", toggleNav);
 });
+
+// carousel
+
+let currentImageIndex = 0;
+const totalImages = 5;
+
+function showImage(index) {
+  const images = document.querySelectorAll(".image-list");
+  const buttons = document.querySelectorAll(".carousel-indicator-off");
+
+  images.forEach((image, i) => {
+    image.style.display = i === index ? "block" : "none";
+  });
+
+  buttons.forEach((button, i) => {
+    button.classList.toggle("carousel-indicator", i === index);
+  });
+}
+
+function nextImage() {
+  currentImageIndex = (currentImageIndex + 1) % totalImages;
+  showImage(currentImageIndex);
+}
+
+function prevImage() {
+  currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+  showImage(currentImageIndex);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showImage(currentImageIndex);
+});
